bfc28aefb7301000a7844929e3dad0f37a3e6d17
ythosa <vasus714@yandex.ru>
2020-11-04 22:49:31 +0300

    Configuration & connection to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,28 @@
-import { Module } from "@nestjs/common";
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
+const environment = process.env.NODE_ENV || 'development';
+
 @Module({
-  imports: [AuthModule, UserModule]
+  imports: [
+    AuthModule,
+    UserModule,
+
+    ConfigModule.forRoot({
+      envFilePath: `.env.${environment}`,
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRoot(
+      process.env.MONGODB_WRITE_CONNECTION_STRING,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    ),
+  ],
 })
 export class AppModule {}
