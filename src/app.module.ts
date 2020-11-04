4793c70b09ddd7aa26e9b10480ab94f7df2daf6b
ythosa <vasus714@yandex.ru>
2020-11-04 22:37:39 +0300

    Auth and user modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,8 @@
 import { Module } from "@nestjs/common";
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
-@Module({})
+@Module({
+  imports: [AuthModule, UserModule]
+})
 export class AppModule {}
