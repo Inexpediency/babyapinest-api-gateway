b19d9636df2e038979e40dd53a519445a0d1a3d8
ythosa <vasus714@yandex.ru>
2020-11-05 08:41:56 +0300

    User service: find and create methods

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import * as _ from 'lodash';
 
+import { CreateUserDto } from './dto/create-user.dto';
 import { IUser } from './interfaces/user.interface' 
 
 @Injectable()
 export class UserService {
     constructor(@InjectModel('User') private readonly userModel: Model<IUser>) {}
 
-    async create() {
-        return null
+    async create(createUserDto: CreateUserDto, roles: Array<string>): Promise<IUser> {
+        const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hash = await bcrypt.hash(createUserDto.password, salt);
+
+        const createdUser = new this.userModel(_.assignIn(createUserDto, { password: hash, roles }));
+        return await createdUser.save();
     }
 
-    async find() {
-        return null
+    async find(id: string): Promise<IUser> {
+        return await this.userModel.findById(id).exec();
     }
 }
