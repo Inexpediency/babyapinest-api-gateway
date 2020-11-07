b6588b4044d640a1faf7cbd9e5923427efc037c7
ythosa <vasus714@yandex.ru>
2020-11-07 12:31:11 +0300

    Sending messages base elements

---
+++ src/mailer/interfaces/mailer-server.interface.ts
@@ -0,0 +1,3 @@
+export interface IMailerServer {
+    sendMail(payload: string): void
+}
--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import {ConfigService} from '@nestjs/config';
+import {MailDto} from './dto/mail.dto';
 
 @Injectable()
-export class MailerService {}
+export class MailerService {
+    constructor(private readonly configService: ConfigService) {}
+
+    send(mailDto: MailDto): void {
+        const mailerServerUrl = this.configService.get<string>('MAILER_SERVER');
+        
+    }
+}
